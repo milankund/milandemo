251711db43c69781fd808f710324fbc48b8f9d8b
milankund <89679266+milankund@users.noreply.github.com>
2021-08-28 22:30:38 +0530

    DynamicClass

--- sample.php
+++ sample.php
@@ -1,7 +1,19 @@
 <?php
-echo "milan";
 
+// class declearation in dynamic
+class a{
+	private $a;
+	protected $b;
 
+	public function show($a,$b){
+		//return $res;
+
+		$res = $this->a+$this->b;
+	}
+}
+
+$obj = new a();
+echo $obj->show(10,20);
 
 
 
