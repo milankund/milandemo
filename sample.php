81976b077104afaec266ac9780cbb16730a0d118
milankund <89679266+milankund@users.noreply.github.com>
2021-08-28 22:55:59 +0530

    addition

--- sample.php
+++ sample.php
@@ -1,20 +1,31 @@
 <?php
 
-// class declearation in dynamic
-class Calculator{
-	public $a;
-	public $b;
-
-	public function Sum($a){
-		$res = $this->a + $this->b;
-		return $res;
-	}
-}
-
-$obj = new Calculator();
-echo $obj->Sum(10,20);
+// Simple example of class and object
+    class SimpleClass{
 
 
+        var $number1;
+        var $number2;
 
+        public function input_two_no($num1,$num2){
+            $this->number1=$num1;
+            $this->number2=$num2;
+        }
+
+        public function the_sum(){
+            $total = $number1+$number2;
+            return $total;
+        }
+
+        public function output_the_sum(){
+            echo $total;
+        }
+
+        $numbers = new SimpleClass();
+        $numbers->input_two_no(10,20);
+        $numbers->the_sum();
+        $numbers->output_the_sum();
+
+    }
 
 ?>
